d31bf88bf2ccc30de0f1fff57452cae57d3bed6a
Aaron Smith <asmith@SWDV.dom>
2019-02-28 15:26:55 -0700

    added users and login creation. fixed typos and logic errors in foreign keys

--- Disk_Inventory_AS.sql
+++ Disk_Inventory_AS.sql
@@ -64,18 +64,47 @@ GO
 CREATE TABLE disk
 	(
 	disk_id int NOT NULL identity Primary key,
-	description nvarchar(100) not null
+	disk_name nvarchar(100) not null,
+	release_date datetime not null,
+	status_id int not null foreign key references diskStatus(status_id),
+	genre_id int not null foreign key references genre(genre_id),
+	disk_type_id int not null foreign key references diskType(disk_type_id)
 	)
 GO
 
 CREATE TABLE diskHasBorrower
 	(
-	DownloadDate datetime NOT NULL
+	disk_id int NOT NULL references disk(disk_id),
+	borrower_id int NOT NULL references borrower(borrower_id),
+	borrowed_date datetime not null,
+	returned_date datetime null,
+	primary key(disk_id, borrower_id, borrowed_date) 
 	)
 GO
+
 CREATE TABLE diskHasArtist
 	(
-	disk_id int NOT NULL identity Primary key,
-	description nvarchar(100) not null
+	disk_id int NOT NULL references disk(disk_id),
+	artist_id int NOT NULL references artist(artist_id),
+	primary key(disk_id, artist_id)
 	)
 GO
+
+
+use master
+go
+if Suser_id('diskUserAS') is not null
+	drop login diskUserAS
+go
+create login diskUserAS with password = 'Pa$$w0rd',
+		default_database = diskInventoryAS;
+go
+use diskInventoryAS
+go
+if User_id('diskUserAS') is not null
+	drop user diskUserAS
+go
+create user diskUserAS
+go
+alter role db_datareader add member diskUserAS
+go
